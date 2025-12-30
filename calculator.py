cd95e4863e892c7a202efe2054d8ddf65eeb295e
Mahenjeeb <mahenjeeb345@gmail.com>
2025-12-30 11:13:12 +0530

    Type Casting, formatting and functions

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# Type Casting default string input to integer(int)
+a = int(input("What is a? "))
+b = int(input("What is b? "))
+
+print(f"Added {a}, {b} and sum is {a+b} ")
+# Formated to comma between 3 digits, if number of digits are more than 3
+print(f"Added {a}, {b} and sum is {(a+b):,} ")
+
+x = float(input("What is X? "))
+y = float(input("What is Y? "))
+z = x / y;
+# Rounding the floating number to a nearest whole number 
+print(f"{round(z)}")
+# Rounding the number and keep 2 decimal places (if any)
+print(f"{round(z,2)}")
+# short-hand to keep number upto 2 decimal places
+print(f"{(z):.2f}")
---
+++ functions.py
@@ -0,0 +1,7 @@
+def main():
+    name = input("Enter Your Name ")
+    hello(name)
+# Passing default parameter to a function
+def hello(to="World"):
+    print(f"Hello, {to}")
+main()
