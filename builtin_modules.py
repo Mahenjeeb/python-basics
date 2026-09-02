67a2eb7c10ed795d7a1db028b7d6ee35ca929c50
Mahenjeeb <mahenjeeb345@gmail.com>
2026-09-02 23:14:22 +0530

    json and calender

--- builtin_modules.py
+++ builtin_modules.py
@@ -39,3 +39,38 @@ print(sys.orig_argv)
 ########
 # json #
 ########
+
+sampleJson = {
+    "data" : [
+        {
+            "name" : "Mahenjeeb",
+            "age" : 27,
+            "cars" : ["Mercedez", "BMW"]
+        },
+        {
+            "name" : "Peti",
+            "age" : 54,
+            "cars" : ["Tata", "Rolls Royce"]
+        },
+        {
+            "name" : "Tinga Bapa",
+            "age" : 56,
+            "cars" : ["That", "Bugatti"]
+        },
+    ],
+    "version" : 1
+}
+# dumps() Format JSON data and change object type to JSON
+formattedJSON = json.dumps(sampleJson, indent=4)
+print(formattedJSON)
+# loads() Change JSON to Python compitale object
+JSONtoObject = json.loads(formattedJSON)
+print(JSONtoObject)
+
+############
+# calender #
+############
+cal_month = calendar.monthcalendar(2016, 9)
+print(cal_month)
+
+
